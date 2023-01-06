f44a7738a3183ee23b09b992891ceef4bdaf9da4
Leonardo Calsavara <109868003+Leo-Calsavara@users.noreply.github.com>
2023-01-06 15:40:32 -0300

    Adição de parâmetros no main

--- memcache.c
+++ memcache.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include<string.h>
 
 void createmat(int linhas, int colunas, double matriz[linhas][colunas]) {
   srand(time(0)); //garante a aleatoriedade do codigo
@@ -11,30 +12,48 @@ void createmat(int linhas, int colunas, double matriz[linhas][colunas]) {
 }}}
 
 
-int main(/* colocar os parametros l1,c1,c2,l2 e transposta ou normal*/)
+int main(/* colocar os parametros l1,c1,c2,l2 e transposta ou normal*/ int argc, char *argv[])
 {
-    int l1, c1, l2, c2, met;
+    int l1, c1, l2, c2, met_normal, met_transposta;
+    char *met, *normal;
+    normal = "Normal";
+        
 
-    printf("Quantas linhas terá sua matriz 1?\n"); //essa parte tem que por nos parametros do main
-    scanf("%d", &l1);
-    printf("E a matriz 2?\n");
-    scanf("%d", &l2);
-    printf("Quantas colunas terá sua matriz 1?\n");    
-    scanf("%d", &c1);
-    printf("E a matriz 2?\n");
-    scanf("%d", &c2);
-    printf("Qual será o metodo da multiplicação?\ndigite: \n[1] para tradicional [2] para transposta\n");
-    scanf("%d", &met);
+    // printf("Quantas linhas terá sua matriz 1?\n"); //essa parte tem que por nos parametros do main
+    // scanf("%d", &l1);
+    // printf("E a matriz 2?\n");
+    // scanf("%d", &l2);
+    // printf("Quantas colunas terá sua matriz 1?\n");    
+    // scanf("%d", &c1);
+    // printf("E a matriz 2?\n");
+    // scanf("%d", &c2);
+    // printf("Qual será o metodo da multiplicação?\ndigite: \n[1] para tradicional [2] para transposta\n");
+    // scanf("%d", &met);
+    if(argc == 6)//atribuicao dos valores passador nos parametros para suas respectivas variaveis
+    {
+      l1 = atoi(argv[1]);
+      c1 = atoi(argv[2]);
+      l2 = atoi(argv[3]);
+      c2 = atoi(argv[4]);
+      met = argv[5];
+    }
+    else//caso nao seja compativel os paramtros com o desejado 
+    {
+      printf("Erro de parametros, favor verificar!/n");
+      return 0;
+    }
     
     double M1[l1][c1]; //cria a matriz de acordo com o que leu
     double M2[l2][c2];
+    //printf("%d %d %d %d", l1, c1, l2, c2);
     
     if (l1 != c2) //critério de multiplicação, se as linhas de uma mat n é igual a coluna de outra não tem como
         printf("Não é possível multiplicar as matrizes\n");
     else{
         createmat(l1, c1, M1); //gera as matrizes aleatorias
         createmat(l2, c2, M2);
-        if(met == 1){ //metodo normal
+        met_normal = strcmp(met, normal);
+        if(met_normal == 0){ //metodo normal
             
         double MR[c1][l2]; //gera a matriz resposta
           
