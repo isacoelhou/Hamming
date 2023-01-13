354a461a6ae36e3c061afa05aa9de492173a1dee
Leonardo Calsavara <109868003+Leo-Calsavara@users.noreply.github.com>
2023-01-13 11:15:27 -0300

    Adição de multiplicação da matriz transposta
    Também adicionado função para imprimir a matriz, caso necessite, e cálculo de tempo

--- memcache.c
+++ memcache.c
@@ -11,12 +11,25 @@ void createmat(int linhas, int colunas, double matriz[linhas][colunas]) {
       matriz[i][j] = rand(); //chama o random
 }}}
 
+void mostramat(int l, int c, double matriz[l][c])
+{
+  for (int i = 0; i < l; i++) {
+    printf("|");
+    for (int j = 0; j < c; j++) {
+      printf("%10d ", matriz[i][j]);
+}
+printf("|\n");
+
+}
+}
+
 
 int main(/* colocar os parametros l1,c1,c2,l2 e transposta ou normal*/ int argc, char *argv[])
 {
     int l1, c1, l2, c2, met_normal, met_transposta;
     char *met, *normal;
     normal = "Normal";
+    double time_spent = 0.0;
         
 
     // printf("Quantas linhas terá sua matriz 1?\n"); //essa parte tem que por nos parametros do main
@@ -47,32 +60,46 @@ int main(/* colocar os parametros l1,c1,c2,l2 e transposta ou normal*/ int argc,
     double M2[l2][c2];
     //printf("%d %d %d %d", l1, c1, l2, c2);
     
-    if (l1 != c2) //critério de multiplicação, se as linhas de uma mat n é igual a coluna de outra não tem como
+    if (l2 != c1) //critério de multiplicação, se as linhas de uma mat n é igual a coluna de outra não tem como
         printf("Não é possível multiplicar as matrizes\n");
     else{
         createmat(l1, c1, M1); //gera as matrizes aleatorias
         createmat(l2, c2, M2);
         met_normal = strcmp(met, normal);
         if(met_normal == 0){ //metodo normal
-            
+        clock_t begin = clock();
         double MR[c1][l2]; //gera a matriz resposta
           
-          for (int i = 0; i < l1; i++)
-          for (int j = 0; j < c2; j++)
-          for (int k = 0; k < c1; k++)
-          MR[i][j] += M1[i][k]*M2[k][j]; //multiplicação
-       
+          for (int i = 0; i < l1; i++){
+          for (int j = 0; j < c2; j++){
+          for (int k = 0; k < c1; k++){
+            MR[i][j] += M1[i][k]*M2[k][j]; //multiplicação
+          
+        }}}
+        clock_t end = clock();
+       //mostramat(l1, c2, MR);
+       time_spent += (double)(end - begin) / CLOCKS_PER_SEC; //calcula o tempo e divide pela funcao persec para passar para segundos
+       printf("o tempo de execução foi de: %lf\n", time_spent);
+
         }
         else{ //metodo 2
-            
+        double MR[c1][l2];
+        clock_t begin = clock();   
         double MT[c2][l2]; //matriz transposta
         for (int i = 0; i < l2; i++) {
         for (int j = 0; j < c2; j++) {
             MT[j][i] = M2[i][j];}} //troca linhas por colunas
         //falta multiplicar a transposta com a normal
+        for (int i = 0; i < l1; i++){
+          for (int j = 0; j < c2; j++){
+          for (int k = 0; k < c1; k++){
+            MR[i][j] += M1[i][k]*MT[k][j]; //multiplicação
         //calcular o tempo de execução
-        }
-    }
+        }}}
+        clock_t end = clock();
+        time_spent += (double)(end - begin) / CLOCKS_PER_SEC; //calcula o tempo e divide pela funcao persec para passar para segundos
+        printf("o tempo de execução foi de: %lf\n", time_spent);   
+    }}
         
 
     return 0;
